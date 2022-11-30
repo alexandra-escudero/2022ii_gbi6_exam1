610460e37891c22156c4c2ae21fe85619d13639d
EscuderoKimberly <kimberly.escudero@est.ikiam.edu.ec>
2022-11-29 22:21:49 -0500

    EscuderoKimberly ha creado archivos txt

--- code/canceres.sh
+++ code/canceres.sh
@@ -1,1 +1,11 @@
 
+###generador de archivo
+
+primero=`tail -n +2 ../data/cancermine.csv | cut -d "," -f 1 | sort | uniq
+`
+for k in $primero
+ do
+ archivos=`touch ../data/ $k.txt`
+ mv $k.txt ../data
+done
+
